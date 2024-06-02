4d33f69c68d31714f341f1fe911429fcbe91a437
Ashita <you@example.com>
2024-06-02 23:46:12 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import './App.css'; 
+import ReactDOM from 'react-dom';
 import AlertFrequency from './components/BarChartComponent';
 import AlertsByCategory from './components/LineChartComponent';
 import AlertsBySeverity from './components/PieChartComponent';
 
 function App() {
   return (
+    <React.StrictMode>
     <div className="App">
       <AlertFrequency />
       <AlertsByCategory />
       <AlertsBySeverity />
     </div>
+    </React.StrictMode>
   );
 }
 
-export default App;
 
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+export default App;
